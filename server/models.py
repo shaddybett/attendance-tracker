7c5d46518c0fae8242bc62f729b893f849b57dd3
Shaddy <shadybett540@gmail.com>
2024-02-18 21:12:00 +0300

    made table for student

--- server/models.py
+++ server/models.py
@@ -16,9 +16,9 @@ class Teacher(db.Model):
     email = db.Column(db.String,nullable=False)
     password = db.Column(db.String,nullable=False)
 
-# class Student(db.Model):
-#     __tablename__='students'
-#     id = db.Column(db.Integer,primary_key=True)
-#     username = db.Column(db.String,nullable=False)
-#     email = db.Column(db.String,nullable=False)
-#     password = db.Column(db.String,nullable=False)
+class Student(db.Model):
+    __tablename__='students'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String,nullable=False)
+    email = db.Column(db.String,nullable=False)
+    password = db.Column(db.String,nullable=False)
